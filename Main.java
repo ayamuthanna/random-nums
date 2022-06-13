c17f26751372f0beec7f5521decd4cb50927b8ba
Student: Aya Aya <94848837+ayamuthanna@users.noreply.github.com>
2022-06-13 13:28:35 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,20 @@
+class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+
+    System.out.println((int)(Math.random() *24)+97);
+
+    char a = 35;
+
+    System.out.println(a);
+
+    char[] and= {35, 65, 78, 98, 100};
+    
+    String[] ands = new String[5];
+    
+    ands = s.valueOf(and[3]);
+
+    System.out.println(ands);
+
+  }
+}
